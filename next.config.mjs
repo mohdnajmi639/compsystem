8c5eb0ed6c451c686b3426305e888147bc4a1202
amsyar <amiramsyar909@gmail.com>
2026-07-20 02:06:40 +0800

    chore: update Next.js server external packages configuration syntax

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    serverActions: true,
-    serverComponentsExternalPackages: ['cloudinary'],
-  },
+  serverExternalPackages: ['cloudinary'],
 };
 
 export default nextConfig;
